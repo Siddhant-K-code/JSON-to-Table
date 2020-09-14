cb03d39fb308a175b742d1fae971737b842cde9b
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-09-14 09:47:36 +0530

    Github Actions
    Github Actions : JSON Data To HTML Table

---
+++ index.js
@@ -0,0 +1,42 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require("fs");
+const path = require("path");
+const readmeBox = require("readme-box").ReadmeBox;
+const chunk = require("chunk");
+
+const generateCell = (cell) => {
+  //
+};
+
+const generateRow = (row) => {
+  //
+};
+
+(async () => {
+  const githubToken = core.getInput("github-token");
+  const filePath = path.join(
+    process.env.GITHUB_WORKSPACE,
+    core.getInput("json-file-path")
+  );
+  const columns = core.getInput("columns");
+  const data = fs.readFileSync(filePath, "utf8");
+  const json = JSON.parse(data);
+
+  try {
+    const content = chunk(json, columns).map((row) =>
+      generateRow(columns, row)
+    );
+    const table = `<table width="100%">${content.join("")}</table>`;
+
+    await readmeBox.updateSection(table, {
+      owner: process.env.GITHUB_REPOSITORY.split("/")[0],
+      repo: process.env.GITHUB_REPOSITORY.split("/")[1],
+      branch: process.env.GITHUB_REF.split("/")[2],
+      token: githubToken,
+      section: "data-section",
+    });
+  } catch (error) {
+    core.setFailed(JSON.stringify(error));
+  }
+})();
